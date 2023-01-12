6ca302fde1c724c895dcba766a94c074115d4572
Ari <aritheriver@gmail.com>
2023-01-11 17:42:55 -0800

    finished

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity= "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+};
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+    };
+  function wrapAdjective(sign= "*") {
+    return function (wrap = 'special') {
+        return `You are ${sign}${wrap}${sign}!`;
+    }
+  }  ;
+
+
+// function wrapAdjective(sign) {
+//     function () {
+//         const wrap= "special";
+//         console.log(`You are ${sign}${wrap}${sign}`);
+//     }
+// };
+// const encouragingPromptFunction = wrapAdjective("!!!")
